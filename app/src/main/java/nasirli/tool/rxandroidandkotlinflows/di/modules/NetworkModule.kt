1076bf205be71451b6da629579455883021ff06d
Ulvi R. Nasirov <URNasirov@bankofbaku.com>
2025-01-27 12:58:42 +0400

    some minor changes

--- app/src/main/java/nasirli/tool/rxandroidandkotlinflows/di/modules/NetworkModule.kt
+++ app/src/main/java/nasirli/tool/rxandroidandkotlinflows/di/modules/NetworkModule.kt
@@ -6,12 +6,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import nasirli.tool.rxandroidandkotlinflows.data.api.ApiService
-import nasirli.tool.rxandroidandkotlinflows.utils.constants.BASE_URL
 import nasirli.tool.rxandroidandkotlinflows.utils.helpers.AppEnvironment
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.WebSocket
-import okhttp3.WebSocketListener
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
